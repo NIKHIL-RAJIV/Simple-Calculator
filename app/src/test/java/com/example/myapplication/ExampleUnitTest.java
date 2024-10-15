2134947f12a1f33f6a80cc67abc4e294525fc32c
NIKHIL RAJIV <69033215+NIKHIL-RAJIV@users.noreply.github.com>
2024-10-15 13:57:19 +0530

    Add files via upload

---
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(6, 3 + 3);
+    }
+}
